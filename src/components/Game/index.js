32ec1046ca74a93ef141f6672d2a8b4e98503d39
Piotr Tylicki <piotr.tylicki@netguru.co>
2017-12-08 14:20:47 +0100

    add logic

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -5,13 +5,34 @@ import Arena from 'components/Arena';
 
 import { Container } from './styles';
 
+const PLAYER1 = '1';
+const PLAYER2 = '2';
+
 class Game extends Component {
+  state = {
+    [PLAYER1]: 0,
+    [PLAYER2]: 0,
+  }
+
+
+  onHit = hittingPlayer => () => {
+    const { onGameOver } = this.props;
+
+    this.setState(
+      prevState => ({
+        [hittingPlayer]: prevState[hittingPlayer] + 1,
+      }),
+      () => Math.abs(this.state[PLAYER1] - this.state[PLAYER2]) === 20 && onGameOver(hittingPlayer),
+    );
+  }
+
   render() {
+
     return (
       <Container>
-        <HitButton />
-          <Arena />
-        <HitButton />
+        <HitButton onClick={this.onHit(PLAYER1)}/>
+        <Arena />
+        <HitButton onClick={this.onHit(PLAYER2)}/>
       </Container>
     );
   }
