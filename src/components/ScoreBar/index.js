d7ce5bd9c8ebeef47eac8fbcf1d6c8d0dc71e290
Piotr Tylicki <piotr.tylicki@netguru.co>
2017-12-08 16:05:37 +0100

    add score bar

---
+++ src/components/ScoreBar/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { LeftBar, RightBar, BarContainter, OuterBar } from './styles.js'
+
+const ScoreBar = ({ score }) => (
+  <OuterBar>
+    <BarContainter>
+      <LeftBar length={score < 0 ? score : 0}></LeftBar>
+    </BarContainter>
+    <BarContainter>
+      <RightBar length={score}></RightBar>
+    </BarContainter>
+  </OuterBar>
+)
+
+export default ScoreBar;
